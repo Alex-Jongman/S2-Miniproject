7ca94337327509b2e7223ba76e14832879b9203e
Alex Jongman <alex.jongman@hu.nl>
2026-04-02 17:57:45 +0000

    Updated Java files

---
+++ src/main/java/S2/security/CORSResponseFilter.java
@@ -0,0 +1,23 @@
+package S2.security;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CORSResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) {
+        //reflect the origin back. * is not allowed with authentication like JWT or cookies
+        String origin = requestContext.getHeaderString("Origin");
+        if (origin != null) {
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", origin);
+        }
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Headers","origin, content-type, accept, authorization");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
